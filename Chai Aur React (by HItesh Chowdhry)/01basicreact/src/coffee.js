726370e55f44f4d0264addcd3e98c588ba6c00dc
Dibya Jyoti Sarmah <dibyasarmah.work@gmail.com>
2023-10-28 22:47:33 +0530

    Both Approaches Checked

---
+++ Chai Aur React (by HItesh Chowdhry)/01basicreact/src/coffee.js
@@ -0,0 +1,5 @@
+function Coffee() {
+  return <h1> Lets have some cappuccino</h1>;
+}
+
+export default Coffee;
---
+++ Chai Aur React (by HItesh Chowdhry)/01vitereact/src/coffee.jsx
@@ -0,0 +1,5 @@
+function Coffee() {
+  return <h1>Coffee aur Code</h1>;
+}
+
+export default Coffee;
